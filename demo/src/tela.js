17af259d19842626db9f0ea3e4005d90a8528e00
Daviddev1 <daviddev1306@gmail.com>
2021-11-04 12:27:10 -0300

    add projeto principal

---
+++ demo/src/tela.js
@@ -0,0 +1,114 @@
+// metodos estáticos nao podem acessar o 'this', por isso, nao vamos colocar o util no construtor
+const util = Util
+
+const ID_CONTEUDO = "conteudo"
+const ID_BTN_JOGAR = "jogar"
+const ID_MENSAGEM = "mensagem"
+const CLASSE_INVISIVEL = "invisible"
+const ID_CARREGANDO = "carregando"
+const ID_CONTADOR = "contador"
+const ID_BTN_MOSTRAR_TUDO = "mostrarTudo"
+const MENSAGENS = {
+    sucesso: {
+        texto: 'Combinação correta!',
+        classe: 'alert-sucess'
+    },
+    erro: {
+        texto: 'Combinação incorreta!',
+        classe: 'alert-danger'
+    }
+}
+class Tela{
+    static obterCodigoHtml(item) {
+        return `
+        <div class="col-md-3">
+            <div class="card" style="width: 50%;" onclick="window.verificarSelecao('${item.id}', '${item.nome}')">
+                <img src="${item.img}" name= "${item.nome}" class="card-img-top" alt="...">
+                </div> 
+                <br />
+        </div>
+        `
+    }
+    static configurarBotaoVerificarSelecao(funcaoOnClick) {
+        window.verificarSelecao = funcaoOnClick
+    }
+    static alterarConteudoHtml(codigoHtml) {
+        const conteudo = document.getElementById(ID_CONTEUDO)
+        conteudo.innerHTML = codigoHtml // innerhtml é para passar o codigo html
+
+    }
+    // Gerar string pela imagem
+    static gerarStringHtmlPelaImagem(itens) {
+        // para cada item da lista, vai executar a função obterCodigoHtml
+        //Ao final vai concatenar tudo em uma unica String
+        // muda de Array para String
+        return itens.map(Tela.obterCodigoHtml).join('') // O .map vai passar por cada um dos itens e vai chamar a funçao q está dentro dele para cada um dos itens
+        // O .join vai pegar o array e ajuntar ele todo de acordo com o separador que eu escolher
+
+    }
+    static atualizarImagens(itens) {
+        const codigoHtml = Tela.gerarStringHtmlPelaImagem(itens)
+        Tela.alterarConteudoHtml(codigoHtml)
+
+    }
+    static configurarBotaoJogar(funcaoOnClick) {
+        const btnJogar = document.getElementById(ID_BTN_JOGAR)
+        btnJogar.onclick = funcaoOnClick
+    }
+    static exibirHerois(nomeDoHeroi, img) {
+        const elementosHtml = document.getElementsByName(nomeDoHeroi)
+        // para cada elemento encontrado na tela, vamos alterar a imagem para a imagem inicial dele
+        // com o forEach, para cada item, dentro dos () setamos o valor de imagem
+        elementosHtml.forEach(item => (item.src = img))
+    }
+    static async exibirMensagem(sucesso = true) {
+        const elemento = document.getElementById(ID_MENSAGEM)
+        if(sucesso) {
+            elemento.classList.remove(MENSAGENS.erro.classe )
+            elemento.classList.add(MENSAGENS.sucesso.classe)
+            elemento.innerText = MENSAGENS.sucesso.texto
+        }
+        else {
+            elemento.classList.remove(MENSAGENS.sucesso.classe) //é para remover do html a classe sucesso se tiver 
+            elemento.classList.add(MENSAGENS.erro.classe)
+            elemento.innerText = MENSAGENS.erro.texto
+        }
+        elemento.classList.remove(CLASSE_INVISIVEL)
+        await util.timeout(1000)
+        elemento.classList.add(CLASSE_INVISIVEL)
+    }
+
+    static exibirCarregando(mostrar = true) {
+        const carregando = document.getElementById(ID_CARREGANDO)
+        if(mostrar) {
+            carregando.classList.remove(CLASSE_INVISIVEL)
+            return
+        }
+        carregando.classList.add(CLASSE_INVISIVEL)
+    }
+    static iniciarContador() {
+        let contarAte = 3
+        const elementoContador = document.getElementById(ID_CONTADOR)
+        // Vamos substituir o texto começando $$contador segundos onde está o $$contador adicionaremos o valor
+        const identificadorNoTexto = "$$contador"
+        const textoPadrao = `Começando em ${identificadorNoTexto} segundos...`
+        //vamos criar uma função em Linha para atualizar o texto a cada segundo
+        const atualizarTexto = () => 
+        (elementoContador.innerHTML = textoPadrao.replace(identificadorNoTexto, contarAte--))
+
+        atualizarTexto()
+        // a cada segundo, vai chamar a funcao atualizarTexto essa funçao vai substituir o $$contador pelo `contarAte` diminuindo o valor. Depois retornamos o idDoIntervalo para ele mais tarde
+        const idDoIntervalo = setInterval(atualizarTexto, 1000)
+        return idDoIntervalo
+    }
+    static limparContador(idDoIntervalo) {
+        clearInterval(idDoIntervalo) // para parar o intervalo de ficar rodando
+
+        //Deixamos sem texto para nao mostar mais nada
+        document.getElementById(ID_CONTADOR).innerHTML = ""
+    }
+    static configurarBotaoMostrarTudo(funcaoOnClick) {
+        const btnMostrarTudo = document.getElementById(ID_BTN_MOSTRAR_TUDO)
+        btnMostrarTudo.onclick = funcaoOnClick
+    }
+}
